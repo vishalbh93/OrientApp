286d6debde4dcb4ee6117c508e23269b4c4a362a
vishalbh93 <167605096+vishalbh93@users.noreply.github.com>
2025-05-11 12:22:41 +0530

    updates files

--- Scripts/src/index.js
+++ Scripts/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 console.log('JS file loaded');
 
 const App = () => {
-    return <div><h1>Hello World!</h1></div>
+    return <div><h1>Hello World! Vishal</h1></div>
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
