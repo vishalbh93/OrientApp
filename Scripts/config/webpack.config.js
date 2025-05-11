4a1a9b069ade8f32822c15b54afb99a86ba66b73
vishalbh93 <167605096+vishalbh93@users.noreply.github.com>
2025-05-11 11:37:39 +0530

    new project post file

---
+++ Scripts/config/webpack.config.js
@@ -0,0 +1,24 @@
+﻿const path = require("path");
+
+module.exports = {
+    devtool: "source-map", 
+    mode: "development",
+    entry: {
+        index: "./Scripts/src/index.js"
+    },
+    output: {
+        path: path.resolve(__dirname, "../../wwwroot/js"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                use: {
+                    loader: "babel-loader",
+                },
+                test: /\.js$/,
+                exclude: /node-modules/
+            }
+        ]
+    }
+}
---
+++ Scripts/src/index.js
@@ -0,0 +1,9 @@
+﻿import React from 'react';
+import ReactDOM from 'react-dom/client';
+console.log('JS file loaded');
+
+const App = () => {
+    return <div><h1>Hello World!</h1></div>
+};
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
