fd88fc40eef4bddd1979f8fe371a851727c1fbb8
vishalbh93 <167605096+vishalbh93@users.noreply.github.com>
2025-05-17 15:34:07 +0530

    updated package

--- Scripts/src/index.js
+++ Scripts/src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-    return <div><h1>Hello World! Vishal Bhosale XyZ</h1></div>
+    return <div><h1>Hello World! Vishal Bhosale ***XyZ</h1></div>
 };
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
